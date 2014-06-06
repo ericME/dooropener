0ed8ba18d9955cb8799d2abd80188323e129e095
Eric Rouse <eric@vergentengineering.com>
2014-06-05 21:05:48 -0700

    demo code added

--- dooropener.ino
+++ dooropener.ino
@@ -1,14 +1,18 @@
 /*
 Door Opener
+-----------
 Detects daylight and opens a door
+Designed for a chicken coop, but could really be for anything.
 by Eric Rouse
 */
 
 int dirPin = 7;
-int stepperPin = 10;
+int stepPin = 10;
 int daylightPin = 4;
-int daydetectPin = 11;
+int lightDetectedPin = 11;
 int spr = 1600; //steps per rev
+float fwd_revs = 5.5;
+float rev_revs = 4.5;
 bool door_open = false;
 bool m_fwd = true;
 bool m_rev = false;
@@ -16,9 +20,9 @@ bool m_rev = false;
 void setup()
 {
   pinMode(dirPin, OUTPUT);
-  pinMode(stepperPin, OUTPUT);
+  pinMode(stepPin, OUTPUT);
   pinMode(daylightPin, INPUT);
-  pinMode(daydetectPin,OUTPUT);
+  pinMode(lightDetectedPin,OUTPUT);
 
 }
 
@@ -27,9 +31,9 @@ void step(boolean dir, int steps)
   digitalWrite(dirPin, dir);
   delay(50);
   for (int i=0;i<steps;i++){
-    digitalWrite(stepperPin, HIGH);
+    digitalWrite(stepPin, HIGH);
     delayMicroseconds(100);
-    digitalWrite(stepperPin, LOW);
+    digitalWrite(stepPin, LOW);
     delayMicroseconds(100);
   }
 }
@@ -37,20 +41,34 @@ void step(boolean dir, int steps)
 void loop()
 {
 
-  //if it's daylight and the door isn't open, open the door
+  //if it is daylight and the door isn't open, open the door
   if (digitalRead(daylightPin) == LOW && door_open == false){
-    digitalWrite(daydetectPin, HIGH);
-    step(m_fwd,spr*5);
+    digitalWrite(lightDetectedPin, HIGH);
+    step(m_fwd,spr*fwd_revs);
     door_open = true;
   }
+  //demo only, remove before production
+  if (digitalRead(8) == HIGH){
+    for (int i = 0; i < 100; i++){
+      digitalWrite(stepPin, HIGH);
+      digitalWrite(dirPin, LOW);
+      delay(100);
+      digitalWrite(stepPin, LOW);
+      digitalWrite(dirPin, HIGH);
+      delay(500);
+    }
+  }
+  delay(1000);
+  
   //if it isn't daylight and the door is open, shut the door
   if(digitalRead(daylightPin) == HIGH && door_open == true){
-    digitalWrite(daydetectPin, LOW);
-    step(m_rev,spr*5);
-    door_open = false;    
+    digitalWrite(lightDetectedPin, LOW);
+    step(m_rev,spr*rev_revs);
+    door_open = false;
+    
   }
 
-  //only check every half hour or so
-  delay(1000);
+  //only check every half hour or so 1800000
+  //delay(1800000);
 
 }
